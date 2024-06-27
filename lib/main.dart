39314063605f90225f86cfe9c2ddc0be222eb5bf
simajune <tejayjung@gmail.com>
2024-06-27 23:29:18 +0900

    3.0 강의 완료

--- lib/main.dart
+++ lib/main.dart
@@ -12,22 +12,44 @@ void main() {
   runApp(App());
 }
 
-
-
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO : impletment build
     // throw UnimplementedError();
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          centerTitle: false,
-          title: Text('Hello Fultter!'),
-        ),
-        body: Center(
-          child: Text('Hello world!'),
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w800),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(0.8), fontSize: 18),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );

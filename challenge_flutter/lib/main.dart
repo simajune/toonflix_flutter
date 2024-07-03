9efaae3e16bb460b441d3d660328e3f6653cccdd
정태준 <taejune.jung@cjconnect.io>
2024-07-03 10:23:08 +0900

    flutter 챌린지 3일차 완료

--- challenge_flutter/lib/main.dart
+++ challenge_flutter/lib/main.dart
@@ -1,6 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var name = 'nico';
-  print("Hi! My name is $name.toUpperCase()!");
+  print(sayHello2(
+    name: 'tj',
+    age: 38,
+    country: 'Korea',
+  ));
 }
+
+String sayHello(String name) {
+  return 'Hello $name nice to meet you!';
+}
+
+String sayHello1(String name) => 'Hello $name nice to meet you!';
+
+num plus(num a, num b) => a + b;
+
+String sayHello2({
+  required String name,
+  required int age,
+  required String country,
+}) {
+  return 'Hello, $name, you are $age, and you come from $country';
+}
+
+String sayHello4(
+  String name,
+  int age,
+  String country,
+) =>
+    'Hello, $name, you are $age, and you come from $country';
+
+String sayHello5(String name, int age, [String? country = 'korea']) =>
+    'Hello, $name, you are $age, and you come from $country';
+
+String capitalizeName(String name) => name.toUpperCase();
+
+String capitalizeName1(String? name) => name?.toUpperCase() ?? "ANON";
+
+ListOfInts reverseListOfNumbers(ListOfInts list) {
+  var reversed = list.reversed;
+  return reversed.toList();
+}
+
+typedef ListOfInts = List<int>;

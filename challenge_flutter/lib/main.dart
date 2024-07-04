8f4917d489d72e78ad488d5c7bfbeb3a259f4ae7
정태준 <taejune.jung@cjconnect.io>
2024-07-04 11:27:19 +0900

    4.8 강의 완료

--- challenge_flutter/lib/main.dart
+++ challenge_flutter/lib/main.dart
@@ -1,85 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+class Human {
+  final String name;
 
-String sayHello(String name) {
-  return 'Hello $name nice to meet you!';
-}
-
-String sayHello1(String name) => 'Hello $name nice to meet you!';
-
-num plus(num a, num b) => a + b;
-
-String sayHello2({
-  required String name,
-  required int age,
-  required String country,
-}) {
-  return 'Hello, $name, you are $age, and you come from $country';
-}
-
-String sayHello4(
-  String name,
-  int age,
-  String country,
-) =>
-    'Hello, $name, you are $age, and you come from $country';
-
-String sayHello5(String name, int age, [String? country = 'korea']) =>
-    'Hello, $name, you are $age, and you come from $country';
-
-String capitalizeName(String name) => name.toUpperCase();
-
-String capitalizeName1(String? name) => name?.toUpperCase() ?? "ANON";
-
-ListOfInts reverseListOfNumbers(ListOfInts list) {
-  var reversed = list.reversed;
-  return reversed.toList();
-}
-
-typedef ListOfInts = List<int>;
-
-class Player {
-  String name;
-  XPLevel xp;
-  Team team;
-
-  Player({
-    required this.name,
-    required this.xp,
-    required this.team,
-  });
-
-  Player.fromJson(Map<String, dynamic> json)
-      : name = json["name"],
-        xp = json["xp"],
-        team = json["team"];
+  Human({required this.name});
 
   void sayHello() {
     print('Hi my name is $name');
   }
-
-  void walk() {}
 }
 
 enum Team { red, blue }
 
-enum XPLevel {
-  beginner,
-  medium,
-  pro,
+class Player extends Human {
+  final Team team;
+
+  Player({
+    required this.team,
+    required String name,
+  }) : super(name: name);
+
+  @override
+  void sayHello() {
+    super.sayHello();
+    print('and I play for $team');
+  }
 }
 
-// abstract class Human {
-//   void walk();
-// }
-
-class Human {
-  final String name;
-
-  Human(this.name);
-
-  void sayHello() {
-    print('Hi my name is $name');
-  }
+void main() {
+  var player = Player(team: Team.blue, name: 'tj');
 }
